4a5e0256c52366113d987bc5f72aa0da86698623
Jens Astrup <jensaiden@gmail.com>
2024-06-17 19:44:13 -0400

    Update tests

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -3,11 +3,18 @@ const mockApp = {
 }
 
 jest.mock('@sb/app', () => mockApp)
-
+jest.mock('datadog-winston')
+jest.mock('winston', () => ({
+  createLogger: jest.fn().mockReturnValue({
+    add: jest.fn()
+  }),
+  format: {
+    json: jest.fn()
+  }
+}))
 
 describe('server', () => {
   it('should listen on a port', () => {
-
     require('@sb/server')
 
     expect(mockApp.listen).toHaveBeenCalled()
