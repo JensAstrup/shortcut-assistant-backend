422eb2954a375236cba5d5300b25d675511866bf
Jens Astrup <JensAstrup@users.noreply.github.com>
2024-06-19 03:41:47 -0400

    Configuration that works with Digital Ocean's oddities (#20)

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,24 @@
+import { config } from 'dotenv'
 import { DataSource } from 'typeorm'
 
 import { User } from '@sb/entities/User'
 import { Workspace } from '@sb/entities/Workspace'
 
 
+config()
+
+
 const database = new DataSource({
   type: 'postgres',
   synchronize: true,
-  url: process.env.DATABASE_URL,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOSTNAME,
+  database: process.env.DB_NAME,
+  port: parseInt(<string>process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: [
     User, Workspace
   ],
