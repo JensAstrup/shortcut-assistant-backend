0f884ad6960db8a5856b4be2cbb3e087e31b6659
Jens Astrup <jensaiden@gmail.com>
2024-06-18 23:26:50 -0400

    Remove unused code from testing

--- src/controllers/openAi.ts
+++ src/controllers/openAi.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from 'express'
 import OpenAI from 'openai'
 
-import database from '@sb/db'
-import { User } from '@sb/entities/User'
 import { logger } from '@sb/utils/logger'
 
 import { StatusCodes } from '../types/status-codes'
