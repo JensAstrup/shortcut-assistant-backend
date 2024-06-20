946bcb27e8004756d6335dc6aeafef26d6bdf9d5
Jens Astrup <jensaiden@gmail.com>
2024-06-19 23:53:21 -0400

    Restore standard log, dynamically determine location of migrations

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,14 @@
+import { join } from 'path'
+
 import { config } from 'dotenv'
 import { DataSource } from 'typeorm'
 
 
 config()
 
+// I really do not like this solution for handling the paths, any suggestions?
+const isDevelopment = process.env.NODE_ENV === 'development'
+const rootPath = isDevelopment ? 'src' : 'dist/src'
 
 const database = new DataSource({
   type: 'postgres',
@@ -14,8 +19,8 @@ const database = new DataSource({
   database: process.env.DB_NAME,
   port: parseInt(<string>process.env.DB_PORT),
   ssl: process.env.NODE_ENV === 'development' ? false : { rejectUnauthorized: false },
-  migrations: ['src/migrations/**/*.ts'],
-  entities: ['src/entities/**/*.ts'],
+  migrations: [join(__dirname, '..', rootPath, 'migrations', '**', '*.{ts,js}')],
+  entities: [join(__dirname, '..', rootPath, 'entities', '**', '*.{ts,js}')],
 })
 
 export default database
