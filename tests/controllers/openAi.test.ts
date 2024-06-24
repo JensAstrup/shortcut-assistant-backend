9a5a1bd386cf97bc9fa31c57ace2f64c13d01ed0
Jens Astrup <JensAstrup@users.noreply.github.com>
2024-06-24 12:05:38 -0400

    Properly handle error states (#30)

--- tests/controllers/openAi.test.ts
+++ tests/controllers/openAi.test.ts
@@ -1,6 +1,12 @@
+import OpenAI from 'openai'
+import { Chat } from 'openai/resources'
+import { Stream } from 'openai/streaming'
 import request from 'supertest'
 
 import app from '@sb/app'
+import { StatusCodes } from '@sb/types/status-codes'
+
+import ChatCompletionChunk = Chat.ChatCompletionChunk
 
 
 jest.mock('openai')
@@ -8,58 +14,85 @@ jest.mock('@sentry/node')
 
 
 
-
-jest.mock('openai', () => {
-  const mockCreate = jest.fn().mockImplementation(() => {
-    const mockStream = (function* (): Generator<{ choices: [{ delta: { content: string } }] }> {
-      yield { choices: [{ delta: { content: 'response from OpenAI' } }] }
-    })()
-    return mockStream
-  })
-
-  return jest.fn().mockImplementation(() => {
-    return {
-      chat: {
-        completions: {
-          create: mockCreate
-        }
-      }
-    }
-  })
-})
+const mockedOpenAI = OpenAI as jest.Mocked<typeof OpenAI>
 
 
 describe('processAnalysis', () => {
-  it('should respond with a 400 if content is missing', async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { status, body } = await request(app).post('/api/openai').send({ promptType: 'analyze' })
+  it('should end response if content is missing', async () => {
+    const mockCreate = jest.fn().mockImplementation(() => {
+      const mockStream = (function* (): Generator<{ choices: [{ delta: { content: string } }] }> {
+        yield { choices: [{ delta: { content: 'response from OpenAI' } }] }
+      })()
+      return mockStream
+    })
+    mockedOpenAI.prototype.chat = {
+      completions: {
+        create: mockCreate
+      },
+    } as unknown as Chat
+    const { status } = await request(app).post('/api/openai').send({ promptType: 'analyze' })
     expect(status).toBe(400)
-    expect(body).toHaveProperty('error')
-    expect(body.error).toBe('Both content and promptType are required fields')
   })
 
-  it('should respond with a 400 if promptType is missing', async () => {
-    const { status, body } = await request(app).post('/api/openai').send({ content: 'test' })
+  it('should end response if prompt is missing', async () => {
+    const mockCreate = jest.fn().mockImplementation(() => {
+      const mockStream = (function* (): Generator<{ choices: [{ delta: { content: string } }] }> {
+        yield { choices: [{ delta: { content: 'response from OpenAI' } }] }
+      })()
+      return mockStream
+    })
+    mockedOpenAI.prototype.chat = {
+      completions: {
+        create: mockCreate
+      },
+    } as unknown as Chat
+    const { status } = await request(app).post('/api/openai').send({ promptType: 'analyze' })
     expect(status).toBe(400)
-    expect(body).toHaveProperty('error')
-    expect(body.error).toBe('Both content and promptType are required fields')
-  })
-
-  it('should respond with a 400 if promptType is invalid', async () => {
-    const { status, body } = await request(app).post('/api/openai').send({ content: 'test', promptType: 'invalid' })
-    expect(status).toBe(400)
-    expect(body).toHaveProperty('error')
-    expect(body.error).toBe('Invalid prompt type')
   })
 
   it('should respond with a 204 if the request is valid', async () => {
+    const mockCreate = jest.fn().mockImplementation(() => {
+      const mockStream = (function* (): Generator<{ choices: [{ delta: { content: string } }] }> {
+        yield { choices: [{ delta: { content: 'response from OpenAI' } }] }
+      })()
+      return mockStream
+    })
+    mockedOpenAI.prototype.chat = {
+      completions: {
+        create: mockCreate
+      },
+    } as unknown as Chat
     const { status } = await request(app).post('/api/openai').send({ content: 'test', promptType: 'analyze' })
     expect(status).toBe(200)
   })
 
   it('properly sets headers and status', async () => {
+    const mockCreate = jest.fn().mockImplementation(() => {
+      const mockStream = (function* (): Generator<{ choices: [{ delta: { content: string } }] }> {
+        yield { choices: [{ delta: { content: 'response from OpenAI' } }] }
+      })()
+      return mockStream
+    })
+    mockedOpenAI.prototype.chat = {
+      completions: {
+        create: mockCreate
+      },
+    } as unknown as Chat
     const { headers, status } = await request(app).post('/api/openai').send({ content: 'test', promptType: 'analyze' })
     expect(headers['content-type']).toBe('text/plain')
     expect(status).toBe(200)
+  })
+
+  it('should respond with 500 if stream is undefined', async () => {
+    mockedOpenAI.prototype.chat = {
+      completions: {
+        create: jest.fn().mockResolvedValue(undefined as unknown as Stream<ChatCompletionChunk>),
+      },
+    } as unknown as Chat
+
+    const { status, body } = await request(app).post('/api/openai').send({ content: 'test', promptType: 'analyze' })
+
+    expect(status).toBe(StatusCodes.SERVER_ERROR)
+    expect(body.error).toBe('Failed to create stream')
   })
 })
