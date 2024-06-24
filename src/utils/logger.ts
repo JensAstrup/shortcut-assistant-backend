ce7ebde0be3c99d33b25b7bf4a8df337985f7cfc
Jens Astrup <jensaiden@gmail.com>
2024-06-24 17:59:32 -0400

    Refactor logging system and add package.json resolution
    The logging system in logger.ts was adapted to provide more detailed metadata and handle console logs based on the environment setting. The tsconfig.json file was updated to allow JSON file resolution.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,21 @@
+import * as process from 'node:process'
+
 import DatadogWinston from 'datadog-winston'
-import { Logger, createLogger, format } from 'winston'
+import { Logger, createLogger, format, transports } from 'winston'
+
+import { version } from '../../package.json'
 
 
 const logger: Logger = createLogger({
   level: 'debug',
   format: format.json(),
-  defaultMeta: { service: 'backend' },
+  defaultMeta: { service: 'backend', version },
 })
+
+const environment = process.env.NODE_ENV || 'development'
+if ((!environment.includes('production') && !environment.includes('staging')) || process.env.CONSOLE_LOGS === 'true') {
+  logger.add(new transports.Console())
+}
 
 logger.add(
   new DatadogWinston({
@@ -15,7 +24,7 @@ logger.add(
     service: 'backend',
     ddsource: 'nodejs',
     intakeRegion: 'us5',
-    ddtags: `env:${process.env.NODE_ENV},version:${process.env.VERSION}`,
+    ddtags: `env:${process.env.NODE_ENV},version:${version}`,
   })
 )
 
