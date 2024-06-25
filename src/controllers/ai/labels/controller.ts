f44bdadfc5bca5be77839332c5f59be7009376c0
Jens Astrup <jensaiden@gmail.com>
2024-06-25 07:55:32 -0400

    Retrieve label suggestions and save to story

---
+++ src/controllers/ai/labels/controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express'
+import { Client } from 'shortcut-api'
+
+import getLabelsFromGPT from '@sb/controllers/ai/labels/get-labels-from-gpt'
+import getUser from '@sb/controllers/users/utils/get-user'
+import { StatusCodes } from '@sb/types/status-codes'
+import logger from '@sb/utils/logger'
+
+
+interface IncomingLabelRequest extends Request {
+    query: {
+        googleId: string
+        storyId: string
+    }
+}
+
+
+async function retrieveLabels(req: IncomingLabelRequest, res: Response): Promise<void> {
+  const user = await getUser(req.query.googleId)
+  if (!user) {
+    res.status(StatusCodes.UNAUTHORIZED).json({ error: 'User not found' })
+    return
+  }
+  logger.info(`Retrieving labels for story ${req.query.storyId}`)
+  logger.info(`User: ${user.shortcutApiToken}`)
+  const client = new Client(user.shortcutApiToken)
+  const story = await client.stories.get(req.query.storyId)
+  const workspaceLabels = await client.labels.list()
+  const labelNames = workspaceLabels.map(label => label.name)
+  const setLabelNames = await getLabelsFromGPT(labelNames, story.description)
+  const setLabels = workspaceLabels.filter(label => setLabelNames.includes(label.name))
+  story.labels = setLabels
+  logger.info(`Setting labels: ${setLabels.map(label => label.id).join(', ')}`)
+  await story.save()
+  res.status(StatusCodes.OK).json({ setLabels })
+}
+
+export default retrieveLabels
