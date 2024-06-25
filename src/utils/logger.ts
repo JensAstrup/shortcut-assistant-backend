3606dc70237dc169c3ed836826ad969450ba0c2b
Jens Astrup <JensAstrup@users.noreply.github.com>
2024-06-24 20:23:55 -0400

    SHA-218 improve logging (#32)
    * Refactor logging system and add package.json resolution
    
    The logging system in logger.ts was adapted to provide more detailed metadata and handle console logs based on the environment setting. The tsconfig.json file was updated to allow JSON file resolution.
    
    * Set environment variable in Dockerfile and logger
    
    * Extract version during build and set in environment
    
    * Fix tests
    
    * Add test coverage

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,23 @@
-import DatadogWinston from 'datadog-winston'
-import { Logger, createLogger, format } from 'winston'
+import * as process from 'node:process'
 
+import DatadogWinston from 'datadog-winston'
+import { Logger, createLogger, format, transports } from 'winston'
+
+import { version } from '../../package.json'
+
+
+const environment = process.env.NODE_ENV || 'development'
 
 const logger: Logger = createLogger({
   level: 'debug',
   format: format.json(),
-  defaultMeta: { service: 'backend' },
+  defaultMeta: { service: 'backend', version, environment },
 })
+
+const nonLoggingEnvironments = ['production', 'staging', 'test']
+if (!nonLoggingEnvironments.includes(environment) || process.env.CONSOLE_LOGS === 'true') {
+  logger.add(new transports.Console())
+}
 
 logger.add(
   new DatadogWinston({
@@ -15,7 +26,7 @@ logger.add(
     service: 'backend',
     ddsource: 'nodejs',
     intakeRegion: 'us5',
-    ddtags: `env:${process.env.NODE_ENV},version:${process.env.VERSION}`,
+    ddtags: `env:${process.env.NODE_ENV},version:${version}`,
   })
 )
 
