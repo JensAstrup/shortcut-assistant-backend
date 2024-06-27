311c33b4c857614702c2bab3d45942805f279bb3
Jens Astrup <jensaiden@gmail.com>
2024-06-27 01:18:47 -0400

    Fallback to userId for refreshed auth tokens

--- src/controllers/users/utils/get-user.ts
+++ src/controllers/users/utils/get-user.ts
@@ -1,13 +1,27 @@
+import googleAuthenticate from '@sb/controllers/users/utils/google-authenticate'
 import database from '@sb/db'
 import { User } from '@sb/entities/User'
 import UserDoesNotExistError from '@sb/errors/user-does-not-exist'
 
 
 async function getUser(googleAuthToken: string): Promise<User> {
+  let user: User | null
   const userRepository = database.getRepository(User)
-  const user = await userRepository.findOne({ where: { googleAuthToken } })
+  user = await userRepository.findOne({ where: { googleAuthToken } })
 
   if (!user) {
+    const userInfo = await googleAuthenticate(googleAuthToken)
+    const googleUserId = userInfo.sub
+    if (googleUserId) {
+      user = await userRepository.findOne({ where: { googleId: googleUserId } })
+      if (!user) {
+        throw new UserDoesNotExistError()
+      }
+      else {
+        user.googleAuthToken = googleAuthToken
+        await userRepository.save(user)
+      }
+    }
     throw new UserDoesNotExistError()
   }
 
