4c2e4f1852ff72dbd46fa0ae7b9cec99de5aae7a
Jens Astrup <jensaiden@gmail.com>
2024-07-09 23:56:55 -0400

    Tidy up db configuration

--- src/db.ts
+++ src/db.ts
@@ -1,22 +1,25 @@
 import { join } from 'path'
 
+import { config } from 'dotenv'
 import { DataSource } from 'typeorm'
 
 
-// I really do not like this solution for handling the paths, any suggestions?
-const isDevelopment = process.env.NODE_ENV === 'development'
-const getPath = (folder: string): string => join(__dirname, isDevelopment ? '../src' : '../dist', folder, '**', '*.{ts,js}')
+config()
 
-const database = new DataSource({
+const isDevelopment = process.env.NODE_ENV === 'development'
+const basePath = isDevelopment ? 'src' : 'dist'
+
+const dataSource = new DataSource({
   type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOSTNAME,
   database: process.env.DB_NAME,
-  port: parseInt(<string>process.env.DB_PORT),
-  ssl: process.env.NODE_ENV === 'development' ? false : { rejectUnauthorized: false },
-  migrations: [getPath('migrations')],
-  entities: [getPath('entities')],
+  ssl: isDevelopment ? false : { rejectUnauthorized: false },
+  entities: [join(__dirname, `../${basePath}/entities/**/*.{ts,js}`)],
+  migrations: [join(__dirname, `../${basePath}/migrations/**/*.{ts,js}`)],
+  logging: true,
 })
 
-export default database
+export default dataSource
