3aa2c243018f70af907858ba68e21b32d96fe46f
Jens Astrup <jensaiden@gmail.com>
2024-07-11 23:46:39 -0400

    Do not overwrite labels

--- src/controllers/ai/labels/set-labels.ts
+++ src/controllers/ai/labels/set-labels.ts
@@ -4,7 +4,7 @@ import { Client, Label } from 'shortcut-api'
 async function setLabels(storyId: string, setLabels: Label[]): Promise<void> {
   const client = new Client()
   const story = await client.stories.get(storyId)
-  story.labels = setLabels
+  story.labels = story.labels.concat(setLabels)
   await story.save()
 }
 
